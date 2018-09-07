f1464977e8bd9a1f2a373aff39188c80e2565d05
Alan Sebastian <asebastian2@gmail.com>
2018-09-07 08:53:47 -0700

    Add v2 bootstrap

--- main.go
+++ main.go
@@ -10,10 +10,13 @@ import (
 	cq "cps/api/conqueso"
 	health "cps/api/health"
 	props "cps/api/properties"
+	v2props "cps/api/v2/properties"
 	kv "cps/pkg/kv"
 	consul "cps/watchers/consul"
 	file "cps/watchers/file"
 	s3 "cps/watchers/s3"
+	v2file "cps/watchers/v2/file"
+	v2s3 "cps/watchers/v2/s3"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -65,43 +68,64 @@ func main() {
 	viper.SetDefault("consul.enabled", true)
 	consulEnabled := viper.GetBool("consul.enabled")
 
-	if fileEnabled {
-		log.Print("File mode is enabled, disabling s3 and consul watchers")
-		s3Enabled = false
-		consulEnabled = false
-		go file.Poll(directory, account, region)
-	}
-
-	if s3Enabled {
-		go s3.Poll(bucket, bucketRegion)
-	}
-
-	if consulEnabled {
-		go consul.Poll(consulHost)
-	} else {
-		kv.WriteProperty("consul", make(map[string][]string))
-	}
+	viper.SetDefault("api.version", 1)
+	apiVersion := viper.GetInt("api.version")
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/v1/properties/{service}", func(w http.ResponseWriter, r *http.Request) {
-		props.GetProperties(w, r, account, region)
-	}).Methods("GET")
+	if apiVersion == 2 {
+		router.HandleFunc("/v2/properties/{scope:.*}", func(w http.ResponseWriter, r *http.Request) {
+			v2props.GetProperties(w, r, account, region)
+		}).Methods("GET")
 
-	router.HandleFunc("/v1/conqueso/{service}", func(w http.ResponseWriter, r *http.Request) {
-		cq.GetConquesoProperties(w, r, account, region)
-	}).Methods("GET")
+		if fileEnabled {
+			log.Print("File mode is enabled, disabling s3 and consul watchers")
+			s3Enabled = false
+			consulEnabled = false
+			go v2file.Poll(directory, account, region)
+		}
 
-	router.HandleFunc("/v1/properties/{service}/{property}", func(w http.ResponseWriter, r *http.Request) {
-		props.GetProperty(w, r, account, region)
-	}).Methods("GET")
+		if s3Enabled {
+			go v2s3.Poll(bucket, bucketRegion)
+		}
+	} else {
+		if fileEnabled {
+			log.Print("File mode is enabled, disabling s3 and consul watchers")
+			s3Enabled = false
+			consulEnabled = false
+			go file.Poll(directory, account, region)
+		}
 
-	router.HandleFunc("/v1/conqueso/{service}", cq.PostConqueso).Methods("POST")
+		if s3Enabled {
+			go s3.Poll(bucket, bucketRegion)
+		}
 
-	// Health returns detailed information about CPS health.
-	router.HandleFunc("/v1/health", health.GetHealth).Methods("GET")
-	// Healthz returns only basic health.
-	router.HandleFunc("/v1/healthz", health.GetHealthz).Methods("GET")
+		if consulEnabled {
+			go consul.Poll(consulHost)
+		} else {
+			kv.WriteProperty("consul", make(map[string][]string))
+		}
+
+		router.HandleFunc("/v1/properties/{service}", func(w http.ResponseWriter, r *http.Request) {
+			props.GetProperties(w, r, account, region)
+		}).Methods("GET")
+
+		router.HandleFunc("/v1/conqueso/{service}", func(w http.ResponseWriter, r *http.Request) {
+			cq.GetConquesoProperties(w, r, account, region)
+		}).Methods("GET")
+
+		router.HandleFunc("/v1/properties/{service}/{property}", func(w http.ResponseWriter, r *http.Request) {
+			props.GetProperty(w, r, account, region)
+		}).Methods("GET")
+
+		router.HandleFunc("/v1/conqueso/{service}", cq.PostConqueso).Methods("POST")
+
+		// Health returns detailed information about CPS health.
+		router.HandleFunc("/v1/health", health.GetHealth).Methods("GET")
+		// Healthz returns only basic health.
+		router.HandleFunc("/v1/healthz", health.GetHealthz).Methods("GET")
+
+	}
 
 	// Serve it.
 	log.Print(http.ListenAndServe(":9100", router))
